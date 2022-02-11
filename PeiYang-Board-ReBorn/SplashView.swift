d9c0561b9c249130d8d5e946025a9cc0142b07d4
bigshezhang <bigshezhang@gmail.com>
2022-02-11 11:29:26 +0800

    Fresh Start

---
+++ PeiYang-Board-ReBorn/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  PeiYang-Board-ReBorn
+//
+//  Created by 李子鸣 on 2022/2/11.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
